3039061ca239b4c94066dbfbf87b839e1896ff03
Tim Felgentreff <timfelgentreff@gmail.com>
2011-10-11 16:31:06 -0700

    make sure we don't accidentally use Ruby::Reflection anything

--- lib/maglev/reflection/object_mirror.rb
+++ lib/maglev/reflection/object_mirror.rb
@@ -1,6 +1,18 @@
 module Maglev
   class Reflection
     class ObjectMirror < Ruby::Reflection::ObjectMirror
+      reflect! Object
+
+      private
+      def field_mirrors(list, subject = @subject)
+        list.collect do |name|
+          field_mirror(subject, name)
+        end
+      end
+
+      def field_mirror(subject, name)
+        Mirror.reflect FieldMirror::Field.new(subject, name)
+      end
     end
   end
 end
