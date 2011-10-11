bb448839ef624bff66e9112b7e6fdd5e0336cd6b
Tim Felgentreff <timfelgentreff@gmail.com>
2011-10-11 16:30:34 -0700

    work on the thread mirror

--- lib/maglev/reflection/thread_mirror.rb
+++ lib/maglev/reflection/thread_mirror.rb
@@ -1,6 +1,15 @@
+require 'maglev/reflection/core_ext/thread'
+
 module Maglev
   class Reflection
     class ThreadMirror < Ruby::Reflection::ThreadMirror
+      reflect! Thread
+      
+      def stack
+        self.__stack_depth.times.collect do |idx|
+          StackFrameMirror.reflect(self.__method_at(idx + 1), idx + 1, self)
+        end
+      end
     end
   end
 end
--- lib/ruby/reflection/thread_mirror.rb
+++ lib/ruby/reflection/thread_mirror.rb
@@ -3,6 +3,26 @@ module Ruby
     class ThreadMirror < ObjectMirror
       include AbstractReflection::ThreadMirror
       reflect! Thread
+
+      def status
+        s = @subject.status
+        if s.respond_to? :to_str
+          s.to_str
+        elsif s.nil?
+          "aborted"
+        else
+          "dead"
+        end
+      end
+
+      def run
+        @subject.run
+      end
+
+      def return_value
+        return nil if @subject.alive?
+        @subject.value
+      end
     end
   end
 end
