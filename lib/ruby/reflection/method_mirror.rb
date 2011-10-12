769f07c9db4fb7ccf333421f253754ece277a037
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-10-11 20:38:41 -0700

    implement Ruby::MethodMirror#file

--- lib/ruby/reflection/method_mirror.rb
+++ lib/ruby/reflection/method_mirror.rb
@@ -3,6 +3,10 @@ module Ruby
     class MethodMirror < Mirror
       include AbstractReflection::MethodMirror
       reflect! Method, UnboundMethod
+
+      def file
+        source_location.first
+      end
     end
   end
 end
