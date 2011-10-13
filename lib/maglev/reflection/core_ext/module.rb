21116c6e2900b090142f677c431b9d34e1b03645
Tim Felgentreff <timfelgentreff@gmail.com>
2011-10-13 13:25:20 -0700

    Fix the transient vs persistent namespace handling

--- lib/maglev/reflection/core_ext/module.rb
+++ lib/maglev/reflection/core_ext/module.rb
@@ -1,10 +1,35 @@
+require 'maglev/orderedcollection'
+
 class Module
   RubyNameSpace = __resolve_smalltalk_global(:RubyNameSpace)
-  RubyNameSpace.primitive 'parent', 'parent'
-  RubyNameSpace.primitive 'my_class', 'myClass'
-  RubyNameSpace.primitive 'keys', 'keys'
-  RubyNameSpace.primitive 'values', 'values'
-  RubyNameSpace.primitive '[]', 'at:'
+  RubyTransientNameSpace = __resolve_smalltalk_global(:RubyTransientNameSpace)
+
+  class RubyNameSpace
+    primitive 'parent', 'parent'
+    primitive 'my_class', 'myClass'
+    primitive 'keys', 'keys'
+    primitive 'values', 'values'
+    primitive '[]', 'at:'
+  end
+
+  class RubyTransientNameSpace < RubyNameSpace
+    primitive 'parent', 'parent'
+    primitive 'persistent_copy', 'persistentCopy'
+
+    def keys
+      super + (persistent_copy ? persistent_copy.keys : [])
+    end
+
+    def values
+      super.to_a + (persistent_copy ? persistent_copy.values.to_a : [])
+    end
+
+    def [](other)
+      super || (persistent_copy ? persistent_copy[other] : nil)
+    rescue Exception
+      nil
+    end
+  end
 
   primitive '__transient_namespace', 'transientNameSpace:'
   primitive 'singleton_class?', 'isRubySingletonClass'
